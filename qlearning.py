97240ec8569a744adb7a11295963bf1054f26cd0
Eloiza <eloiza.rossetto@gmail.com>
2021-02-22 16:49:59 -0300

    Funcao atualizaAgente e escolheAcao implementadas

--- qlearning.py
+++ qlearning.py
@@ -11,23 +11,31 @@ class Agente():
         self.env = env #ambiente
 
         #initiates the Q-Table
-        n_actions = 4       #total actions - move up, right, left, down
         n_states  = 16      #total states - every different cell in the game   
+        n_actions = 4       #total actions - move up, right, left, down
         self.qTable = np.zeros((n_states, n_actions))
-
+        
     '''
     Based on a state returns an action - e-greedy policy 
     '''
     def escolheAcao(self, state):
         actions = self.env.getLegalActions(state)
-
-        #perform the best action
+        
+        #perform the best action - exploitation
         if(random.uniform(0, 1) > self.epsilon):
-            action = np.argmax(self.qTable[state])
+            best_action = (0, self.qTable[state][0]) #tuple to represent (best_action, q_value)
+           
+            #searches the best action, with the highest Q-value in the table
+            for action in actions:
+                if(self.qTable[state, action] > best_action[1]):
+                    best_action = (action, self.qTable[state, action])
 
-        #take a random action 
+            # action = np.argmax(self.qTable[state])
+            action = int(best_action[0]) 
+        
+        #take a random action - exploration
         else:
-           action = random.choice(actions)
+            action = random.choice(actions)
         
 
         return action
@@ -36,8 +44,9 @@ class Agente():
     Updates the Q-value of the chosen action
     '''
     def atualizaAgente(self, state, action, reward, next_state):
-        self.qTable[state, action] = (1 - self.alpha)*self.qTable[state,action] + self.alpha*(reward + self.gamma * np.max(self.qTable[next_state,:]) - self.qTable[state,action])
-
+        sample = reward + self.gamma * np.max(self.qTable[next_state,:])
+        self.qTable[state, action] = (1 - self.alpha)*self.qTable[state,action] + self.alpha*sample
+    
     '''
     Metodo chamado ao fim de cada episodio.
     A variavel is_train ira indicar se eh um episodio de treinamento
