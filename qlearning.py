82f36c4ef2dac0e936227a66d6d6d38b093ce7ff
Eloiza <eloiza.rossetto@gmail.com>
2021-02-22 15:00:37 -0300

    Re-implementando com funcoes do numpy

--- qlearning.py
+++ qlearning.py
@@ -11,59 +11,37 @@ class Agente():
         self.env = env #ambiente
 
         #initiates the Q-Table
-        self.qTable = []
         n_actions = 4       #total actions - move up, right, left, down
         n_states  = 16      #total states - every different cell in the game   
-        for i in range(n_actions):
-            self.qTable.append([0 for i in range(n_states)])
+        self.qTable = np.zeros((n_states, n_actions))
 
     '''
     Based on a state returns an action - e-greedy policy 
     '''
     def escolheAcao(self, state):
         actions = self.env.getLegalActions(state)
-        action = random.choice(actions)
-
-        # print("Actions:", actions)
-        # print("Action:", action)
-        # print("State:", state)
-        # self.env.env.render()
-        # print()
 
         #perform the best action
-        if(random.random() > self.epsilon):
-            best_action = (0,0) #tuple to represent (best_action, q_value)
-            
-            #searches the best action, with the highest Q-value in the table
-            for action in range(len(self.qTable)):
-                for state in range(len(self.qTable[i])):
-                    if(qTable[action][state] > best_action):
-                        best_action = (best_action, qTable[action][state])
-
-            action = best_action[0]
+        if(random.uniform(0, 1) > self.epsilon):
+            action = np.argmax(self.qTable[state])
 
         #take a random action 
         else:
-           action  = random.choice(actions)
+           action = random.choice(actions)
         
+
         return action
            
     '''
-    Updates the Q-value of the choosen action
+    Updates the Q-value of the chosen action
     '''
     def atualizaAgente(self, state, action, reward, next_state):
-        #atualiza os Q-values definidos de acordo com a regra de atualização do alg
-        #Equacoes 1 e 2 representam as regras de atualizacao 
+        self.qTable[state, action] = (1 - self.alpha)*self.qTable[state,action] + self.alpha*(reward + self.gamma * np.max(self.qTable[next_state,:]) - self.qTable[state,action])
 
-        q_value = qTable[action][state]
-        sample = reward + self.gamma * max(0, self.qTable[action, next_state])
-
-        self.qTable[action][state] = (1 - self.alpha)*q_value + self.alpha*sample
-
-
+    '''
+    Metodo chamado ao fim de cada episodio.
+    A variavel is_train ira indicar se eh um episodio de treinamento
+    '''
     def novoEpisodio(self, is_train=True):
-        '''
-        Metodo chamado ao fim de cada episodio.
-        A variavel is_train ira indicar se eh um episodio de treinamento
-        '''
+        
         pass
