50f141ad7d46bbdeb4782d4de495e2648d7ba5db
Eloiza <eloiza.rossetto@gmail.com>
2021-02-22 11:52:06 -0300

    função escolheAcao implementada - inicio da implementação da função atualizaAgente

--- qlearning.py
+++ qlearning.py
@@ -13,7 +13,7 @@ class Agente():
         #initiates the Q-Table
         self.qTable = []
         n_actions = 4       #total actions - move up, right, left, down
-        n_states  = 4       #total states - starting position, frozen cell, hole, goal position   
+        n_states  = 16      #total states - every different cell in the game   
         for i in range(n_actions):
             self.qTable.append([0 for i in range(n_states)])
 
@@ -22,10 +22,25 @@ class Agente():
     '''
     def escolheAcao(self, state):
         actions = self.env.getLegalActions(state)
-        
+        action = random.choice(actions)
+
+        # print("Actions:", actions)
+        # print("Action:", action)
+        # print("State:", state)
+        # self.env.env.render()
+        # print()
+
         #perform the best action
         if(random.random() > self.epsilon):
-            action = ...
+            best_action = (0,0) #tuple to represent (best_action, q_value)
+            
+            #searches the best action, with the highest Q-value in the table
+            for action in range(len(self.qTable)):
+                for state in range(len(self.qTable[i])):
+                    if(qTable[action][state] > best_action):
+                        best_action = (best_action, qTable[action][state])
+
+            action = best_action[0]
 
         #take a random action 
         else:
@@ -34,16 +49,17 @@ class Agente():
         return action
            
     '''
-    Metodo chamado apos executar cada acao durante o treinamento
+    Updates the Q-value of the choosen action
     '''
     def atualizaAgente(self, state, action, reward, next_state):
         #atualiza os Q-values definidos de acordo com a regra de atualização do alg
         #Equacoes 1 e 2 representam as regras de atualizacao 
 
-        #(1) amostra = R(s,a,s') + gamma*maxQ(s', a')
-        #(2) Temporal DIfference or TD-Update -> Q(s,a) = (1-alpha)*Q(s,a)+alpha*amostra
+        q_value = qTable[action][state]
+        sample = reward + self.gamma * max(0, self.qTable[action, next_state])
 
-        pass
+        self.qTable[action][state] = (1 - self.alpha)*q_value + self.alpha*sample
+
 
     def novoEpisodio(self, is_train=True):
         '''
